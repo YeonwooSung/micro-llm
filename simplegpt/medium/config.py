cd7652efa27b0beec480cfe7f465fc751e411c8d
YeonwooSung <neos960518@gmail.com>
2022-12-31 16:40:24 +0900

    Get rid of small models from medium config dict

--- simplegpt/medium/config.py
+++ simplegpt/medium/config.py
@@ -10,9 +10,4 @@ CONFIG_DICT = {
     'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
     # Gophers
     'gopher-44m':   dict(n_layer=8, n_head=16, n_embd=512),
-    # (there are a number more...)
-    # I made these tiny models up
-    'gpt-mini':     dict(n_layer=6, n_head=6, n_embd=192),
-    'gpt-micro':    dict(n_layer=4, n_head=4, n_embd=128),
-    'gpt-nano':     dict(n_layer=3, n_head=3, n_embd=48),
 }
